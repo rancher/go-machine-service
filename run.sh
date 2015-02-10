2820623fb6956ed301b7b72610ad3d3c839ba9a1
Craig Jellick <craig@rancher.com>
2015-02-10 12:18:47 -0700

    Moved run script

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-go clean; go build
-CATTLE_ACCESS_KEY="" CATTLE_SECRET_KEY="" CATTLE_URL=http://localhost:8080/v1 CATTLE_URL_FOR_AGENT=http://10.0.2.2:8080 nohup ./go-machine-service &
---
+++ tools/local-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+go clean; go build
+
+# Asssumes you have a default service account type setup
+CATTLE_AGENT_LOCALHOST_REPLACE="10.0.2.2" CATTLE_ACCESS_KEY="service" CATTLE_SECRET_KEY="servicepass" CATTLE_URL=http://localhost:8080/v1 ./go-machine-service

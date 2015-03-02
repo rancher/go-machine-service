7a6f719ddf6c2c367584307423277cc034ff5459
Craig Jellick <craig@rancher.com>
2015-03-02 06:31:33 -0700

    Fix docker connection parsing
    docker-machine updated it's connection config output such that the -H
    parameter is no longer wrapped with quotes. This fixes that and remans
    backwards compatible with the version where the -H param is quoted.

--- handlers/helpers_test.go
+++ handlers/helpers_test.go
@@ -13,11 +13,23 @@ func TestParseConnectionArgs(t *testing.T) {
 	key := "/foo/bar/key.pem"
 	endpoint := "tcp://127.0.0.1:2376"
 
+	// "Normal"
 	testArgs := fmt.Sprintf("--tls --tlscacert=%v   --tlscert=%v --tlskey=%v -H=\"%v\" ",
 		ca, cert, key, endpoint)
 	testParse(testArgs, ca, cert, key, endpoint, t)
 
+	// -H at beginning
 	testArgs = fmt.Sprintf("-H=\"%v\" --tlscacert=%v --tlscert=%v --tlskey=%v --tls",
+		endpoint, ca, cert, key)
+	testParse(testArgs, ca, cert, key, endpoint, t)
+
+	// -H at end with no quotes
+	testArgs = fmt.Sprintf("--tls --tlscacert=%v   --tlscert=%v --tlskey=%v -H=%v",
+		ca, cert, key, endpoint)
+	testParse(testArgs, ca, cert, key, endpoint, t)
+
+	// -H at beginning with no quotes
+	testArgs = fmt.Sprintf("-H=%v --tlscacert=%v --tlscert=%v --tlskey=%v --tls",
 		endpoint, ca, cert, key)
 	testParse(testArgs, ca, cert, key, endpoint, t)
 

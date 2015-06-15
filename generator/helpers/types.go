145c30392eed31db11e443a478211936a2e5bd0a
sidharthamani <sid@rancher.com>
2015-06-15 15:33:46 -0700

    make generator generate documentation as well

--- generator/helpers/types.go
+++ generator/helpers/types.go
@@ -6,12 +6,24 @@ type ResourceFieldConfig struct {
 	Required bool   `json:"required,omitempty"`
 }
 
+type DocumentationFieldConfig struct {
+	Description string `json:"description,omitempty"`
+}
+
+type DocumentationFields struct {
+	Id             string                    `json:"id,omitempty"`
+	ResourceFields DocumentationFieldConfigs `json:"resourceFields,omitempty"`
+}
+
 type ResourceData struct {
-	Blacklist   []string
-	Drivers     []string
-	ResourceMap map[string]ResourceFields
+	Blacklist        []string
+	Drivers          []string
+	ResourceMap      map[string]ResourceFields
+	DocumentationMap map[string][]DocumentationFields
 }
 
 type ResourceFieldConfigs map[string]ResourceFieldConfig
 
 type ResourceFields map[string]ResourceFieldConfigs
+
+type DocumentationFieldConfigs map[string]DocumentationFieldConfig

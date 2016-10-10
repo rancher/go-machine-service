8751a649e74a4791dafbb25886fb18073f181750
Darren Shepherd <darren@rancher.com>
2016-10-10 10:49:01 -0700

    Do not make name required

--- dynamic/machine.go
+++ dynamic/machine.go
@@ -57,8 +57,8 @@ func baseSchema(drivers []string, defaultAuth string) client.Schema {
 				Type:     "string",
 				Create:   true,
 				Update:   true,
-				Required: true,
-				Nullable: false,
+				Required: false,
+				Nullable: true,
 			},
 		},
 	}

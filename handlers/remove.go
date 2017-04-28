c0c2bf35cee6124c172be229ef0fca6ade0e33bd
Craig Jellick <craig@rancher.com>
2017-04-27 19:14:00 -0700

    Add cache that records when a machine is removed
    This will prevent long running remove operations from getting into and
    endless remove cycle.

--- handlers/remove.go
+++ handlers/remove.go
@@ -1,16 +1,29 @@
 package handlers
 
 import (
+	"time"
+
 	"github.com/Sirupsen/logrus"
+	"github.com/patrickmn/go-cache"
 	"github.com/rancher/event-subscriber/events"
 	client "github.com/rancher/go-rancher/v2"
 )
+
+var removeCache = cache.New(5*time.Minute, 30*time.Second)
 
 func PurgeMachine(event *events.Event, apiClient *client.RancherClient) error {
 	logger.WithFields(logrus.Fields{
 		"resourceId": event.ResourceID,
 		"eventId":    event.ID,
 	}).Info("Purging Machine")
+
+	if _, ok := removeCache.Get(event.ResourceID); ok {
+		logger.WithFields(logrus.Fields{
+			"resourceId": event.ResourceID,
+			"eventId":    event.ID,
+		}).Info("Machine already purged")
+		return publishReply(newReply(event), apiClient)
+	}
 
 	machine, machineDir, err := preEvent(event, apiClient)
 	if err != nil || machine == nil {
@@ -28,6 +41,8 @@ func PurgeMachine(event *events.Event, apiClient *client.RancherClient) error {
 			return err
 		}
 	}
+
+	removeCache.Add(event.ResourceID, true, cache.DefaultExpiration)
 
 	logger.WithFields(logrus.Fields{
 		"resourceId":        event.ResourceID,

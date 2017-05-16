9243ed67b2876391b80734d80dfb51f22f04aa2b
Ivan Mikushin <i.mikushin@gmail.com>
2017-05-16 12:03:07 -0700

    WIP: fake tests (skip this)

--- handlers/create-event_test.go
+++ handlers/create-event_test.go
@@ -10,22 +10,34 @@ import (
 	"time"
 )
 
-var eventFlavorHostCreate = &events.Event{
-	Name:         "physicalhost.create;handler=goMachineService",
-	ID:           "873c98c6-50c3-4e5e-b3ca-aa37f4020588",
-	ReplyTo:      "reply.4427210190781767296",
-	ResourceID:   "1ph7",
-	ResourceType: "physicalHost",
+var (
+	eventFlavorHostCreate = &events.Event{
+		Name:         "physicalhost.create;handler=goMachineService",
+		ID:           "873c98c6-50c3-4e5e-b3ca-aa37f4020588",
+		ReplyTo:      "reply.4427210190781767296",
+		ResourceID:   "1ph7",
+		ResourceType: "physicalHost",
 
-	Data: map[string]interface{}{
-		"name":          "ivan-do1",
-		"kind":          "machine",
-		"externalId":    "8bb270e3-5920-412a-a4c4-3951e16ccbe9",
-		"rancherConfig": map[string]interface{}{"flavor": "digitalocean-sfo2-2gb"},
-		"accountId":     5,
-		"hostname":      "ivan-do1",
-	},
-}
+		Data: map[string]interface{}{
+			"name":          "ivan-do1",
+			"kind":          "machine",
+			"externalId":    "8bb270e3-5920-412a-a4c4-3951e16ccbe9",
+			"rancherConfig": map[string]interface{}{"flavor": "digitalocean-sfo2-2gb"},
+			"accountId":     5,
+			"hostname":      "ivan-do1",
+		},
+	}
+
+	eventFlavorHostDelete1 = &events.Event{
+		Name:         "physicalhost.remove;handler=goMachineService",
+		ID:           "a439dbf2-7657-46ae-931a-e1b400f80fe0",
+		ReplyTo:      "reply.4074420224106915443",
+		ResourceID:   "1ph6",
+		ResourceType: "physicalHost",
+
+		Data: map[string]interface{}{},
+	}
+)
 
 var apiClient, err = client.NewRancherClient(&client.ClientOpts{
 	Timeout:   time.Second * 30,
@@ -39,6 +51,20 @@ func TestCreateMachine(t *testing.T) {
 
 	err := CreateMachine(eventFlavorHostCreate, apiClient)
 	assert.Nil(err)
+}
+
+func TestDeleteMachine1(t *testing.T) {
+	assert := require.New(t)
+
+	err := PurgeMachine(eventFlavorHostDelete1, apiClient)
+	assert.Nil(err)
+}
+
+func TestMachineExists(t *testing.T) {
+	assert := require.New(t)
+
+	exists, _ := machineExists("/Users/ivan/.cattle/machine/machines/fdccb274-92b2-4e77-ad79-f0a772622e80", "ivan-do1")
+	assert.False(exists)
 }
 
 var fields = map[string]interface{}{
